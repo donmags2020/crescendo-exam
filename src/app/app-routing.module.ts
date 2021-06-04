e7aff8be2e8f710ef815b03ae32950bb25e8796f
donmags2020 <60498947+donmags2020@users.noreply.github.com>
2021-06-04 09:28:10 +0800

    Update on view recipe
    Update on view recipe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsrecipeComponent } from './detailsrecipe/detailsrecipe.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'detailsrecipe', component: DetailsrecipeComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
